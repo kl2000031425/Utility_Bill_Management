e254236c7f25b5669e9d50fcf4c13cd9f404b172
@kl2000031425 <2000031425@kluniversity.in>
2024-03-24 02:47:33 +0530

    add files via upload

--- src/server/app.js
+++ src/server/app.js
@@ -4,9 +4,9 @@ const stripe = require('stripe')('your_secret_key');
 
 const app = express();
 
-// This is your real test secret API key.
-const YOUR_SECRET_KEY = 'sk_test_51OxYnESBXtkb1tVSWMKPwosg9Yw5nWarhMloq3Mu0Z4PnjnzYQb3bHh08M7a3iHvl1VuwpQRCfs6hESabUG1uZ55004wGRy396';
-const YOUR_PUBLIC_KEY = 'pk_test_51OxYnESBXtkb1tVSYs4G4EL6OTObmzxU0N4BIpz6M2XB8QeYzwTkHm8ZJVyPvBSm7AtQq6mlIydu62ONjWpn3zuG00UBokOHUQ';
+
+const YOUR_SECRET_KEY = '';
+const YOUR_PUBLIC_KEY = '';
 
 stripe.apiKey = YOUR_SECRET_KEY;
 
@@ -16,7 +16,7 @@ app.use(express.json());
 app.post('/create-payment-intent', async (req, res) => {
   const { items } = req.body;
   
-  // Create a PaymentIntent with the order amount and currency
+
   const paymentIntent = await stripe.paymentIntents.create({
     amount: calculateOrderAmount(items),
     currency: "usd"
@@ -32,10 +32,6 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 function calculateOrderAmount(items) {
-  // This function should calculate the amount based on items in the cart
-  
-  // Replace this constant with a calculation of the order's amount
-  // For simplicity of this example, it's a hardcoded amount.
   const amount = 100;
   return amount;
 }
